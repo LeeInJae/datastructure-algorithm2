1720dfaaf5e187cc9afd477d982004ac9b44f78f
LeeInJae <cowspirit@naver.com>
2014-11-18 11:31:19 +0900

    add hw2 c3, c4

--- HW2 C4/HW2 C4/QuicSort.cpp
+++ HW2 C4/HW2 C4/QuicSort.cpp
@@ -39,78 +39,74 @@ void PrintArr(int * arr, int size){
 }
 
 int Partition(int * arr, int left, int right){
-	int pivot = arr[right-1];
+	int pivot = arr[right];
 	int wall = -1;
 
-	for (int i = 0; i < right - 1; ++i){
+	for (int i = 0; i <= right - 1; ++i){
 		if (arr[i] <= pivot){
 			++wall;
 			Swap(&arr[wall], &arr[i]);
 		}
 	}
 	++wall;
-	Swap(&arr[wall], &arr[right-1]);
+	Swap(&arr[wall], &arr[right]);
 	return wall;
 }
 
-int TestPartition(int * arr, int size, int pivotPos){
-	for (int i = 0; i < pivotPos; ++i){
+int TestPartition(int * arr, int left, int right, int pivotPos){
+	for (int i = left; i < pivotPos; ++i){
 		if (arr[i] > arr[pivotPos])
 			return 0;
 	}
 
-	for (int i = pivotPos + 1; i < size; ++i){
+	for (int i = pivotPos + 1; i <= right; ++i){
 		if (arr[pivotPos] > arr[i])
 			return 0;
 	}
 	return 1;
 }
 
+void QuickSort(int * arr, int left, int right){
+	if (left >= right) return;
+	
+	if (left < right){
+		int pos = Partition(arr, left, right);
+		if (TestPartition(arr, left, right, pos)){
+			QuickSort(arr, left, pos - 1);
+			QuickSort(arr, pos + 1, right);
+		}
+		else
+			return;
+	}
+}
+
 int main(void){
 	int * arr;
 	int pos;
-	
-	for (int i = 1; i <= 32; ++i){
+
+	for (int i = 1; i <= 16; ++i){
 		arr = Init(i);
-		pos = Partition(arr, 0, i);
 		PrintArr(arr, i);
-		if (TestPartition(arr, i, pos)){
-			printf("pivot의 위치는 %d 이고, 파티션이 아주 잘 되었음\n", pos);
-		}
-		else
-		{
-			printf("파티션이 아주 잘 못되었음\n");
-		}
+		QuickSort(arr, 0, i - 1);
+		PrintArr(arr, i);
 		delete arr;
-		
+
 		printf("\n\n");
-		
+
 		arr = InitIncrease(i);
-		pos = Partition(arr, 0, i);
 		PrintArr(arr, i);
-		if (TestPartition(arr, i, pos)){
-			printf("pivot의 위치는 %d 이고, 파티션이 아주 잘 되었음\n", pos);
-		}
-		else
-		{
-			printf("파티션이 아주 잘 못되었음\n");
-		}
+		QuickSort(arr, 0, i - 1);
+		PrintArr(arr, i);
 		delete arr;
 
 		printf("\n\n");
 
 		arr = InitDecrease(i);
-		pos = Partition(arr, 0, i);
 		PrintArr(arr, i);
-		if (TestPartition(arr, i, pos)){
-			printf("pivot의 위치는 %d 이고, 파티션이 아주 잘 되었음\n", pos);
-		}
-		else
-		{
-			printf("파티션이 아주 잘 못되었음\n");
-		}
+		QuickSort(arr, 0, i - 1);
+		PrintArr(arr, i);
 		delete arr;
-		
+
 		printf("\n\n");
 	}
 
