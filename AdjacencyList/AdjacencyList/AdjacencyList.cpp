1991727c7a60b2cafa90db01b60b5e7aa4e7b8c6
LeeInJae <cowspirit@naver.com>
2015-01-15 22:57:34 +0900

    벨만이형

--- AdjacencyList/AdjacencyList/AdjacencyList.cpp
+++ AdjacencyList/AdjacencyList/AdjacencyList.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #define VertexNum 5
-#define EdgeNum 9
+#define EdgeNum 10
 #define INF 10000000
 
 using namespace std;
@@ -15,6 +15,7 @@ typedef struct _Vertex{
 
 typedef struct _SingleLinkedList{
 	Vertex * head;
+	int elementNumber;
 }SingleList;
 
 typedef struct _Edge{
@@ -31,6 +32,7 @@ bool ArrAdjInit(SingleList * arrAdj){
 		dummyNode->nodeId = -1;
 		dummyNode->weight = 0;
 		arrAdj[i].head = dummyNode;
+		arrAdj[i].elementNumber = 0;
 	}
 	return true;
 }
@@ -58,13 +60,12 @@ bool InsertAdjVer(SingleList * arrAdj, int u, int  v, int weight){
 		newNode->next = arrAdj[u].head;
 	}
 	else{
-		newNode->next = headNextNode;
-		newNode->prev = arrAdj[u].head;
-		headNextNode->prev = newNode;
-		arrAdj[u].head->next = newNode;
-		arrAdj[u].head->prev = newNode->next;
-		headNextNode->next = arrAdj[u].head;
+		arrAdj[u].head->prev->next = newNode;
+		newNode->prev = arrAdj[u].head->prev;
+		arrAdj[u].head->prev = newNode;
+		newNode->next = arrAdj[u].head;
 	}
+	++arrAdj[u].elementNumber;
 	return true;
 }
 
@@ -91,15 +92,16 @@ bool InitEdgeList(Edge * edgeList){
 	if (edgeList == nullptr)
 		return false;
 
-	edgeList[0].e = 0, edgeList[0].e = 1, edgeList[0].e = 6;
-	edgeList[1].e = 0, edgeList[1].e = 3, edgeList[1].e = 7;
-	edgeList[2].e = 1, edgeList[2].e = 2, edgeList[2].e = 5;
-	edgeList[3].e = 1, edgeList[3].e = 4, edgeList[3].e = -4;
-	edgeList[4].e = 1, edgeList[4].e = 3, edgeList[4].e = 8;
-	edgeList[5].e = 2, edgeList[5].e = 1, edgeList[5].e = -2;
-	edgeList[6].e = 3, edgeList[6].e = 4, edgeList[6].e = 9;
-	edgeList[7].e = 4, edgeList[7].e = 0, edgeList[7].e = 2;
-	edgeList[8].e = 4, edgeList[8].e = 2, edgeList[8].e = 7;
+	edgeList[0].s = 0, edgeList[0].e = 1, edgeList[0].weight = 6;
+	edgeList[1].s = 0, edgeList[1].e = 3, edgeList[1].weight = 7;
+	edgeList[2].s = 1, edgeList[2].e = 2, edgeList[2].weight = 5;
+	edgeList[3].s = 1, edgeList[3].e = 4, edgeList[3].weight = -4;
+	edgeList[4].s = 1, edgeList[4].e = 3, edgeList[4].weight = 8;
+	edgeList[5].s = 2, edgeList[5].e = 1, edgeList[5].weight = -2;
+	edgeList[6].s = 3, edgeList[6].e = 4, edgeList[6].weight = 9;
+	edgeList[7].s = 4, edgeList[7].e = 0, edgeList[7].weight = 2;
+	edgeList[8].s = 4, edgeList[8].e = 2, edgeList[8].weight = 7;
+	edgeList[9].s = 3, edgeList[9].e = 2, edgeList[9].weight = -3;
 	return true;
 }
 
@@ -107,17 +109,30 @@ bool BellmanFord(SingleList * arrAdj, Edge * edgeList, int * dist){
 	if (arrAdj == nullptr || edgeList == nullptr)
 		return false;
 
-	for (int i = 0; i < EdgeNum; ++i){
-		if (edgeList[i].weight >= INF) continue;
-		if (dist[edgeList[i].s] >= INF) continue;
+	for (int k = 0; k < VertexNum; ++k){
+		for (int i = 0; i < EdgeNum; ++i){
+			if (edgeList[i].weight >= INF) continue;
+			if (dist[edgeList[i].s] >= INF) continue;
 
-		if (dist[edgeList[i].e] > dist[edgeList[i].s] + edgeList[i].weight){
-			dist[edgeList[i].e] = dist[edgeList[i].s] + edgeList[i].weight;
+			if (dist[edgeList[i].e] > dist[edgeList[i].s] + edgeList[i].weight){
+				dist[edgeList[i].e] = dist[edgeList[i].s] + edgeList[i].weight;
+			}
 		}
 	}
 	return true;
 }
 
+void PrintEdgeList(SingleList * arrAdj){
+	for (int i = 0; i < VertexNum; ++i){
+		cout << i << "->  ";
+		Vertex * curNode = arrAdj[i].head->next;
+		for (int j = 0; j < arrAdj[i].elementNumber; ++j){
+			cout << curNode->nodeId << "   , ";
+			curNode = curNode->next;
+		}
+		cout << endl;
+	}
+}
 int main(void){
 	SingleList * arrAdj = new SingleList[VertexNum];
 	int * dist = new int[VertexNum];
@@ -130,7 +145,7 @@ int main(void){
 	InitEdgeList(edgeList);
 	
 	EdgeSetting(arrAdj, edgeList);
-	
+	PrintEdgeList(arrAdj);
 	BellmanFord(arrAdj, edgeList, dist);
 	getchar();
 	delete[] arrAdj;
